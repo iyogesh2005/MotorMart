a4c08c3c4379729cc8132f3d79858a19378c2482
Yogesh <iyogesh2005@gmail.com>
2026-03-29 12:03:10 +0530

    Resolved conflicts and updated project

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://motormart-1.onrender.com/api/products", 
 });
 
 API.interceptors.request.use((req) => {
-
   const user = JSON.parse(localStorage.getItem("user"));
 
   if (user?.token) {
